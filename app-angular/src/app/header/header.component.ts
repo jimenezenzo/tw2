614e64732d1fd2d5cf1ac7df585ea4b56d0bc19f
Enzo Jimenez <enjimenez@crjppf.gov.ar>
2022-05-09 11:42:17 -0300

    cambio de plantilla

--- app-angular/src/app/header/header.component.ts
+++ app-angular/src/app/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  cartOpen = false;
+  isOpen = false;
+  
   constructor() { }
 
   ngOnInit(): void {

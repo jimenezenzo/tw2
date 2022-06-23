459dfa91e719bcb5707995c6c3e039fd37ae6eb8
Facundo Marin <facundo.marin@moxit.net>
2022-06-23 00:44:56 -0300

    agrego componente

---
+++ app-angular/src/app/agregar-producto/agregar-producto.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { NgForm } from '@angular/forms';
+import { empty } from 'rxjs';
+
+@Component({
+  selector: 'app-agregar-producto',
+  templateUrl: './agregar-producto.component.html',
+  styleUrls: ['./agregar-producto.component.css']
+})
+export class AgregarProductoComponent implements OnInit {
+
+  detallesProducto: Producto = {
+    id : "0",
+    nombre : "",
+    categoria: "",
+    precio : 0,
+    descripcion: "",
+    almacenamiento: "",
+    color: "",
+    stock: 0,
+    imagen: "",
+    bateria: ""
+
+  }; 
+  cargando: boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  crearProducto (formulario : NgForm){
+    
+  }
+}

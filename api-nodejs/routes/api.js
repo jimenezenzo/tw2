eb72c97517fb17a4c46f90b3bb7e81a62da5659b
Facundo Marin <facundo.marin@moxit.net>
2022-06-27 17:37:35 -0300

    agrego ruta

--- api-nodejs/routes/api.js
+++ api-nodejs/routes/api.js
@@ -1,24 +1,29 @@
-import express from "express";
-import { obtenerProductos, crearProducto, buscarProductos } from "../controllers/ProductosController.js"
-import PaymentController from "../controllers/PaymentsControllers.js";
-import PaymentService from "../services/PaymetService.js";
+import express from 'express';
+import {
+  obtenerProductos,
+  crearProducto,
+  buscarProductos,
+  crearProducto2,
+} from '../controllers/ProductosController.js';
+import PaymentController from '../controllers/PaymentsControllers.js';
+import PaymentService from '../services/PaymetService.js';
 
 const router = express.Router();
-const PaymentInstance = new PaymentController(new PaymentService);
+const PaymentInstance = new PaymentController(new PaymentService());
 
 // endpoints api
-router.get("/productos", obtenerProductos);
-router.post("/producto", crearProducto);
-router.post("/buscar-productos", buscarProductos);
-// router.post("/producto", crearProducto);
-router.get("/crear", crearProducto);
+router.get('/productos', obtenerProductos);
+router.post('/producto', crearProducto);
+router.post('/buscar-productos', buscarProductos);
+router.post('/crear-producto', crearProducto2);
+router.get('/crear', crearProducto);
 
-router.post("/payment", function (req, res, next) {
-    PaymentInstance.getPaymentLink(req, res);
+router.post('/payment', function (req, res, next) {
+  PaymentInstance.getPaymentLink(req, res);
 });
-  
-router.post("/webhook", function (req, res, next) {
-    PaymentInstance.webhook(req, res);
+
+router.post('/webhook', function (req, res, next) {
+  PaymentInstance.webhook(req, res);
 });
 
 export default router;

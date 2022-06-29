bd021421775e5f5000013467f04d273b82c700a3
Facundo Marin <facundo.marin@moxit.net>
2022-06-28 23:19:32 -0300

    Update agregar-producto.component.ts

--- app-angular/src/app/agregar-producto/agregar-producto.component.ts
+++ app-angular/src/app/agregar-producto/agregar-producto.component.ts
@@ -6,31 +6,29 @@ import { ProductosService } from '../services/productos/productos.service';
 @Component({
   selector: 'app-agregar-producto',
   templateUrl: './agregar-producto.component.html',
-  styleUrls: ['./agregar-producto.component.css']
+  styleUrls: ['./agregar-producto.component.css'],
 })
 export class AgregarProductoComponent implements OnInit {
-
   detallesProducto: Producto = {
-    _id : "",
-    nombre : "",
-    categoria: "",
-    precio : 0,
-    descripcion: "",
-    almacenamiento: "",
-    color: "",
+    _id: '',
+    nombre: '',
+    categoria: '',
+    precio: 0,
+    descripcion: '',
+    almacenamiento: '',
+    color: '',
     stock: 0,
-    imagen: "",
-    bateria: ""
+    imagen: '',
+    bateria: '',
+    marca: '',
+  };
+  cargando: boolean = false;
 
-  }; 
-  cargando: boolean = false
+  constructor(private _productoService: ProductosService) {}
 
-   constructor(private _productoService: ProductosService,) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  crearProducto (formulario : NgForm){
+  crearProducto(formulario: NgForm) {
     this._productoService.createProduct(formulario.value).subscribe();
   }
 }
